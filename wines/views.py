8b12595f8cb770c7bf0043a5e8dbdd810556184a
lcarvajal <leonardo.carvajal@webfactura.cl>
2024-09-25 01:43:03 -0300

    test2

--- wines/views.py
+++ wines/views.py
@@ -184,12 +184,9 @@ def scrape_vinoteca():
     # s = Service(r"F:/Coding/freelnce/vinoscl/chromedriver-win64/chromedriver.exe")
     # driver = webdriver.Chrome(service=s)
     options = Options()
-    options.headless = True  # Ejecutar en modo headless
-    options.add_argument("--no-sandbox")
-    options.add_argument("--disable-dev-shm-usage")
-    options.add_argument("--disable-gpu")
-    options.add_argument("--window-size=1920x1080")  # Establecer tamaño de ventana
-    options.add_argument("--remote-debugging-port=9222")  # Puerto para depuración remota
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
 
     # Iniciar el servicio de Chrome
     service = Service('/usr/bin/chromium')
