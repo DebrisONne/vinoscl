57a597e6b51e7e8d52fd32dd37e2c21354ec99b1
lcarvajal <leonardo.carvajal@webfactura.cl>
2024-10-02 13:29:47 -0300

    solo ewine

--- wines/management/commands/scrape_wines.py
+++ wines/management/commands/scrape_wines.py
@@ -5,14 +5,14 @@ class Command(BaseCommand):
     help = 'Comando para iniciar el scraping de vinotecas.'
 
     def handle(self, *args, **options):
-        self.stdout.write("Iniciando el scraping de la vinoteca...")
-        result_vinoteca = scrape_vinoteca()
-        self.stdout.write(f"Scraping de la vinoteca completado: {result_vinoteca}")
+        # self.stdout.write("Iniciando el scraping de la vinoteca...")
+        # result_vinoteca = scrape_vinoteca()
+        # self.stdout.write(f"Scraping de la vinoteca completado: {result_vinoteca}")
 
-        #self.stdout.write("Iniciando el scraping de eWine...")
-        #result_ewine = scrape_ewine()
-        #self.stdout.write(f"Scraping de eWine completado: {result_ewine}")
+        self.stdout.write("Iniciando el scraping de eWine...")
+        result_ewine = scrape_ewine()
+        self.stdout.write(f"Scraping de eWine completado: {result_ewine}")
 
-        self.stdout.write("Iniciando el scraping de Mundo Vino...")
-        result_mundo_vino = scrape_mundo_vino()
-        self.stdout.write(f"Scraping de Mundo Vino completado: {result_mundo_vino}")
+        # self.stdout.write("Iniciando el scraping de Mundo Vino...")
+        # result_mundo_vino = scrape_mundo_vino()
+        # self.stdout.write(f"Scraping de Mundo Vino completado: {result_mundo_vino}")
