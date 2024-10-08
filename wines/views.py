3a859fc7b063693da3d5edfa70dd4e569d24c6cb
lcarvajal <leonardo.carvajal@webfactura.cl>
2024-10-08 02:44:05 -0300

    size

--- wines/views.py
+++ wines/views.py
@@ -257,6 +257,7 @@ def scrape_ewine():
     # driver = webdriver.Chrome(service=s)
     options = Options()
     options.add_argument('--headless')
+    options.add_argument('--window-size=1920x1080')
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
 
